8b18f9e394613007802b0b9b90f4b042218d43a4
=elirehema <=rocksandystone@gmail.com>
2020-01-20 19:50:22 +0300

    Added Sign Views

---
+++ app/src/main/java/com/student/rentals/ui/activities/SignUpActivity.kt
@@ -0,0 +1,46 @@
+package com.student.rentals.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.student.rentals.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        link_login.setOnClickListener(){
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+    /** Make activity visible to the user
+     * Prepare the app to enter foreground and become interactive
+     * **/
+    override fun onStart() {
+        //loadFragment(ViewItemFragment())
+        super.onStart()
+    }
+
+    /** It comes to the foreground
+     * The app is now interactive to user **/
+    override fun onResume() {
+
+        super.onResume()
+    }
+
+    /** User is leaving us **/
+    override fun onPause() {
+        super.onPause()
+    }
+
+    /**Activity is no longer visible to user**/
+    override fun onStop() {
+        super.onStop()
+    }
+
+    /** before activity is destroyed **/
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
