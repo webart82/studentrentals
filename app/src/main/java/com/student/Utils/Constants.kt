1b0384e3627daa020ca9b4001b0753973f21850c
=elirehema <=rocksandystone@gmail.com>
2020-01-26 15:56:20 +0300

    Multiple image select

--- app/src/main/java/com/student/Utils/Constants.kt
+++ app/src/main/java/com/student/Utils/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
       val BASE_URL:String = "http://apartment.aflixea-tz.com/api/"
       var SINGLE_IMAGE: Int = 1
       var SELECT:Char = 'P'
+      var PICK_IMAGE_MULTIPLE: Int =1
 }
 
